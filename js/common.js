210647fe99598f3e0b9d83773b9a03dc50c753c8
thuhoan22 <thuhoan2208@gmail.com>
2023-10-16 17:28:19 +0700

    Home page - done

--- js/common.js
+++ js/common.js
@@ -1,13 +1,6 @@
 ;(function (win, $) {
     'use strict';
 
-	var swiper = new Swiper('.testimonials', {
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	});
-
 	$(window).on('scroll', function(){
 	});
 
---
+++ js/main.js
@@ -0,0 +1,80 @@
+// Swiper
+function swiperTestimonials() {
+	const swiper = new Swiper('.testimonials', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+};
+
+// Hoverring
+function mouseHover() {
+	if (window.innerWidth >= 1024){
+		let itemCursor = document.querySelector('.cursor-custom');
+	
+		document.addEventListener('mousemove', moveCursor);
+	
+		function moveCursor(e) {
+			let x = e.clientX;
+			let y = e.clientY;
+			
+			itemCursor.style.left = `${x}px`;
+			itemCursor.style.top = `${y}px`;
+		}
+	
+		let links = Array.from(document.querySelectorAll('.m-zoom'));
+	
+		links.forEach((link) => {
+			itemCursor.classList.remove('view-project');
+	
+			link.addEventListener('mouseover', () => {
+				itemCursor.classList.add('grow');
+			});
+			link.addEventListener('mouseleave', () => {
+				itemCursor.classList.remove('grow');
+			});
+		})
+	
+		let projects = Array.from(document.querySelectorAll('.card-hover'));
+	
+		projects.forEach((project) => {
+			itemCursor.classList.remove('grow');
+	
+			project.addEventListener('mouseover', () => {
+				itemCursor.classList.add('view-project');
+			});
+			project.addEventListener('mouseleave', () => {
+				itemCursor.classList.remove('view-project');
+			});
+		})
+	}
+};
+
+// Open GNB
+function mobileGnbOpen() {
+	let btnMenu = document.querySelector('.nav .btn-mobile');
+
+	btnMenu.addEventListener('click', () => {
+		let thisExpanedStatus = btnMenu.getAttribute('aria-expanded');
+
+		if (thisExpanedStatus == 'false') {
+			btnMenu.classList.add('is-active');
+			btnMenu.closest('.wrap').classList.add('is-open');
+			btnMenu.setAttribute('aria-expanded', true);
+		} else {
+			btnMenu.classList.remove('is-active');
+			btnMenu.closest('.wrap').classList.remove('is-open');
+			btnMenu.setAttribute('aria-expanded', false);
+		}
+	});
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	swiperTestimonials();
+	mobileGnbOpen();
+});
+
+window.addEventListener('resize', mouseHover);
